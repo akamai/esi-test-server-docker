31a8e2300d6f3249391c5db8bdc29802035ef315
Serguei Joulanov <sjoulano@akamai.com>
2017-07-25 12:25:53 -0400

    added copyright statement/comments

--- code-samples/geo/us/akamai.js
+++ code-samples/geo/us/akamai.js
@@ -1,4 +1,4 @@
-<!-- Copyright 2017 Akamai Technologies, Inc. All Rights Reserved.
+/* Copyright 2017 Akamai Technologies, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,8 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. 
-//  -->
-
+*/
 
 <!--hide this script from non-javascript-enabled browsers
 
