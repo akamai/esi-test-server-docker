10c8482910ed5f0fff2086c277ad7c8d795518aa
Tai Dickerson <tdickers@akamai.com>
2017-08-28 16:37:27 -0400

    WEBPERF-641: add tests to reproduce

---
+++ dockerimage-tests/test/set_headers_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+# Copyright 2017 Akamai Technologies, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require './test/test_helper'
+
+# Tests that the default no-args config works as expected
+class SetHeadersTest < Minitest::Test
+  def setup
+    start_containers
+  end
+
+  def test_can_set_content_type
+    url = "http://#{HOST_HOSTNAME}:#{@esi_port}/advanced/content_type.html"
+    puts "URL: #{url}"
+    response = HTTParty.get(url)
+    assert_equal(200, response.code)
+    assert(response.headers['content-type'] == 'text/esi-html')
+  end
+
+  def test_can_set_etag
+    url = "http://#{HOST_HOSTNAME}:#{@esi_port}/advanced/set_etag.html"
+    puts "URL: #{url}"
+    response = HTTParty.get(url)
+    assert_equal(200, response.code)
+    assert(response.headers['etag'] == 'abc123')
+  end
+
+  def test_can_set_custom_header
+    url = "http://#{HOST_HOSTNAME}:#{@esi_port}/advanced/set_header.html"
+    puts "URL: #{url}"
+    response = HTTParty.get(url)
+    assert_equal(200, response.code)
+    assert(response.headers['customheader'] == 'helloEsi')
+  end
+end
